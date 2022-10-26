297455ebea54e8b4534565c00f5952b0f3b7a19e
Aldy Paskah Sekeon <asekeon07@gmail.com>
2022-10-26 15:21:50 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+  echo "Hello World!<br>";
+
+  $nama = "Aldy";
+  $umur = 20;
+
+  echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun <br>";
+
+  $namaAdik = "Alda";
+  $umurAdik = 14;
+
+  echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun <br>";
+
+  $selisihUmur = $umur - $umurAdik;
+  echo "selisih usia saya dan adik saya adalah $selisihUmur tahun"
+
+
+?>
